c02b8d084ddbf93ab7b4f5e6b6ad6e62b48d4999
Bence Meszaros <bence.me@gmail.com>
2015-10-29 19:20:08 -0400

    Make SErrors extend RuntimeExceptions, append Error to their names

--- src/main/scala/SError.scala
+++ src/main/scala/SError.scala
@@ -1,10 +1,11 @@
 package slisp
 
-sealed trait SError
-case class SNumArgs (expected: Int, found: List[SVal]) extends SError
-case class STypeMismatch (expected: String, found: SVal) extends SError
-case class SParse (error: Error) extends SError
-case class SBadSpecialForm (msg: String, form: SVal) extends SError
-case class SNotAFunction (msg: String, fnc: String) extends SError
-case class SUnboundVar (msg: String, vname: String) extends SError
+import scala.util.parsing.combinator.Parsers
+
+sealed trait SError extends RuntimeException
+case class SParseError (error: Parsers#NoSuccess) extends SError
+case class SNumArgsError (expected: Int, found: List[SVal]) extends SError
+case class STypeMismatchError (expected: String, found: SVal) extends SError
+case class SNotAFunctionError (msg: String, fnc: String) extends SError
+case class SUnboundVarError (msg: String, vname: String) extends SError
 case class SDefaultError (msg: String) extends SError
