4ca1baf9a6a536f653beb98fb8677e96e629b4a6
Bence Meszaros <bence@Bences-MacBook-Pro.local>
2015-10-29 19:24:44 -0400

    Add a few SParser and SEvaluator tests

--- src/test/scala/SEvaluatorTest.scala
+++ src/test/scala/SEvaluatorTest.scala
@@ -3,6 +3,8 @@ package slisp
 import org.scalatest.FunSuite
 
 class EvaluatorTest extends FunSuite {
+	import SLispImplicits._
+
 	def testNum(expr: String)(expected: Double) = {
 		assertResult(SNumber(expected))(SLisp(expr))
 	}
@@ -23,6 +25,22 @@ class EvaluatorTest extends FunSuite {
 		testNum("(+ 1 -2 3)") (1 + -2 + 3)
 	}
 
+	test("Weak equality") {
+		testTrue("""(weak== 1 '1)""")
+		testTrue("""(weak== 1 "1")""")
+		testTrue("""(weak== true 'true)""")
+		testTrue("""(weak== true "true")""")
+
+		testTrue("""(weak== "yes" 'yes)""")
+		testTrue("""(weak== 'yes "yes")""")
+
+		testTrue("""(weak== 1.0 1)""")
+		testTrue("""(weak== 1.0 0.99999999999999999999999999999999999)""")
+
+		testTrue("""(weak== 1 1.0 1 1.0 '1 "1" '1)""")
+		testTrue("""(weak== '1 "1" 1)""")
+	}
+
 	test("Simple string comparison") {
 		testTrue("""(< "a" "b")""")
 		testTrue("""(< "hello world" "hi world")""")
@@ -30,11 +48,25 @@ class EvaluatorTest extends FunSuite {
 	}
 
 	test("Simple conditionals") {
-		testString("""(if (< 1 2) "hi" "IMPOSSIBLE!")""") ("\"hi\"")
-		testString("""(if (> 1 2) "hi" "IMPOSSIBLE!")""") ("\"IMPOSSIBLE!\"")
+		testString("""(if (< 1 2) "hi" "IMPOSSIBLE!")""") ("hi")
+		testString("""(if (> 1 2) "hi" "IMPOSSIBLE!")""") ("IMPOSSIBLE!")
 	}
 
 	test("Simple list primitives") {
 		assertResult(SNumber(1)) (SLisp("(car '(1 2 3))"))
+		assertResult(SList(List(2, 3))) (SLisp("(cdr '(1 2 3))"))
+	}
+
+	test("List `cons`s") {
+		assertResult(SList(List(1, 2, 3))) (SLisp("(cons 1 '(2 3))"))
+		assertResult(SList(List(1))) (SLisp("(cons 1 '())"))
+
+		assertResult(SList(List(1, 2))) (SLisp("(cons 1 2)"))
+	}
+
+	test("Binds") {
+		assertResult(SNumber(1)) (SLisp("(bind ((hi 1) (ho (+ hi 1))) (+ hi ho) (- ho hi))"))
+
+		// assertResult(SNumber(1)) (SLisp("((bind ((hi 1) (ho (+ hi 1))) (+ hi ho) (- ho hi))"))
 	}
 }
--- src/test/scala/SParserTest.scala
+++ src/test/scala/SParserTest.scala
@@ -3,9 +3,14 @@ package slisp
 import org.scalatest.FunSuite
 
 class ParserTest extends FunSuite {
-	test("Simple empty list") {
-		assertResult(SList(List())) {
-			SParser("()")
-		}
+	def escapeOuterList(expected: SVal) (source: String) = 
+		assertResult(SList(List(expected))) (SParser(source))
+
+	test("Empty list") {
+		assertResult(SList(List())) (SParser("()"))
+	}
+
+	test("String without quotes") {
+		escapeOuterList(SString("hi")) ("(\"hi\")")
 	}
 }
