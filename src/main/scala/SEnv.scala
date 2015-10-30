5c812b967b6ae96a3f87e162bf9ad791497b36cc
Bence Meszaros <bence.me@gmail.com>
2015-10-30 01:32:39 -0400

    Change SEnv to use mutable maps

--- src/main/scala/SEnv.scala
+++ src/main/scala/SEnv.scala
@@ -1,13 +1,12 @@
 package slisp
 
-case class SEnv (val scopes: List[Map[Int, SVal]]) {
-	def get (name: String): Option[SVal] = get(name.##)
-	def get (name: Int): Option[SVal] = scopes find (_ contains name) map (_(name))
+case class SEnv (val scopes: List[MutMap[String, SVal]]) {
+	def get(name: String): Option[SVal] = scopes find (_ contains name) map (_(name))
 
-	def newScope: SEnv = SEnv((Map(): Map[Int, SVal]) :: scopes)
+	def newScope: SEnv = SEnv(MutMap[String, SVal]() :: scopes)
 
-	def addToCurrentScope(pair: (Int, SVal)): SEnv = scopes match {
-		case x :: xs => SEnv((x + pair) :: xs)
-		case _ => throw SDefaultError("Unable to add variable to scope (THIS IS BAD)")
+	def addToCurrentScope(pair: (String, SVal)): SEnv = {
+		scopes.head += pair
+		this
 	}
 }
